f2f1c146656b86f76b887382a23ad35dcde096e4
Rares Milea <milearares@yahoo.ro>
2024-12-14 20:14:15 +0100

    just do it brute force lolll

--- Code/day6_2.py
+++ Code/day6_2.py
@@ -1,5 +1,5 @@
-from copy import deepcopy
-
+with open("D:\\Chestii bune\\advent2024\\inputs\\6.txt", "r") as infile:
+    inp = infile.read()
 
 def turn(d):
     turn_dir = {
@@ -25,64 +25,46 @@ def show_mat(mat):
             print(mat[i][j], end='')
         print('')
 
-with open("D:\\Chestii bune\\Advent shit\\inputs\\test.txt", "r") as infile:
-    inp = infile.read()
-
 
 m = [[y for y in x] for x in inp.strip().split('\n')]
-m_copy = deepcopy(m)
+try_walls = []
 for i in range(len(m)):
     for j in range(len(m[0])):
+        if m[i][j] not in ['^', '#']:
+            try_walls.append((i,j))
         if m[i][j] != '^':
             pass
         else:
             start = (i, j)
-            break
 
 i, j = start[0], start[1]
 
 wall_ct = 0
-moves = 0
-turning_dir = {}
-while True:
-    # print(f"MOVE: {moves}")
-    # show_mat(m)
 
-    add_i, add_j = dir_add(m[i][j])
-    if not (0 <= i+add_i < len(m) and 0 <= j+add_j < len(m[0])):
-        m[i][j] = 'X'
-        moves+=1
-        # print('')
-        break
-    if m[i+add_i][j+add_j] == '#' or m[i+add_i][j+add_j] == '$':
-        m[i+add_i][j+add_j] = '$'
-        turning_dir[(i+add_i, j+add_j)] = m[i][j]
-        m[i][j] = turn(m[i][j])
-    else:
-        x, y = deepcopy(i), deepcopy(j)
-        pos_dir_add_x, pos_dir_add_y = dir_add(turn(m[i][j]))
-        x += pos_dir_add_x
-        y += pos_dir_add_y
-        while 0 <= x < len(m) and 0 <= y < len(m[0]):
-            if m[x][y] == '$' and turn(m[i][j]) == turning_dir[(x, y)]:
-                wall_ct += 1
-                print(f"CAN ADD ON: {i, j}")
-                m_copy[i+add_i][j+add_j] = '$'
-                show_mat(m_copy)
-                m_copy[i+add_i][j+add_j] = m[i+add_i][j+add_j] if m[i+add_i][j+add_j] == '#' else '^' if (i+add_i, j+add_j) == (start[0], start[1]) else '.'
+for try_wall in try_walls:
+    m[try_wall[0]][try_wall[1]] = '#'
+    i, j = start[0], start[1]
+    m[i][j] = '^'
+    prev_mat_dir = dict()
 
-                print('')
-                break
-            x += pos_dir_add_x
-            y += pos_dir_add_y
+    while True:
 
-        i, j = i+add_i, j+add_j
-        m[i][j] = m[i-add_i][j-add_j]
-        m[i-add_i][j-add_j] = 'X'
+        if m[i][j] in prev_mat_dir.get((i,j), []):
+            wall_ct+=1
+            break
+        prev_mat_dir.setdefault((i,j), []).append(m[i][j])
 
-    moves+=1
-    # print('')
-# print(f"MOVE: {moves}")
-# show_mat(m)
+        add_i, add_j = dir_add(m[i][j])
+        if not (0 <= i+add_i < len(m) and 0 <= j+add_j < len(m[0])):
+            m[i][j] = '.'
+            break
+        if m[i+add_i][j+add_j] == '#':
+            m[i][j] = turn(m[i][j])
+        else:
+            i, j = i+add_i, j+add_j
+            m[i][j] = m[i-add_i][j-add_j]
+            m[i-add_i][j-add_j] = '.'
+
+    m[try_wall[0]][try_wall[1]] = '.'
 
 print(wall_ct)
